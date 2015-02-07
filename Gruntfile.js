2c072414cbbc634f3259c61da254d97d76fd0f9b
Ben Centra <bcentra@constantcontact.com>
2015-02-07 16:46:39 -0500

    new grunt tasks, switching font to Open Sans

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
 // Project config
 grunt.initConfig({
   pkg: grunt.file.readJSON('package.json'),
+  // Minify CSS
   cssmin: {
     members: {
       options: {
@@ -32,21 +33,66 @@ grunt.initConfig({
       }]
     }
   },
+  // Compile LESS to CSS
   less: {
     members: {
       options: {
-        paths: ["dev/"]
+        paths: ['dev/']
       },
       files: {
-        "dev/members.css": "dev/members.less"
+        'dev/members.css': 'dev/members.less'
       }
     },
     public: {
       options: {
-        paths: ["dev/"]
+        paths: ['dev/']
       },
       files: {
-        "dev/public.css": "dev/public.less"
+        'dev/public.css': 'dev/public.less'
+      }
+    }
+  },
+  // Watch for changes
+  watch: {
+    members: {
+      files: [ 'dev/members.less' ],
+      tasks: [ 'less:members', 'cssmin:members' ]
+    },
+    public: {
+      files: [ 'dev/public.less' ],
+      tasks: [ 'less:public', 'cssmin:public' ]
+    }
+  },
+  // Run a local server
+  connect: {
+    default: {
+      options: {
+        port: 9000,
+        keepalive: true,
+        base: './',
+        hostname: '*'
+      }
+    },
+    members: {
+      options: {
+        port: 9000,
+        keepalive: true,
+        base: './',
+        hostname: '*',
+        open: {
+          target: 'http://localhost:9000/test/members'
+        }
+      }
+    },
+    public: {
+      options: {
+        port: 9000,
+        keepalive: true,
+        base: './',
+        hostname: '*',
+        open: {
+          target: 'http://localhost:9000/test/public'
+        }
       }
     }
   }
@@ -55,8 +101,18 @@ grunt.initConfig({
 // Load plugins
 grunt.loadNpmTasks('grunt-contrib-cssmin');
 grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-watch');
 
 // Register tasks
 grunt.registerTask('default', ['less', 'cssmin']);
+grunt.registerTask('defaultMembers', ['less:members', 'cssmin:members']);
+grunt.registerTask('defaultPublic', ['less:public', 'cssmin:public']);
+grunt.registerTask('dev', ['default', 'watch']);
+grunt.registerTask('devMembers', ['defaultMembers', 'watch:members']);
+grunt.registerTask('devPublic', ['defaultPublic', 'watch:public']);
+grunt.registerTask('test', ['connect:default']);
+grunt.registerTask('testMembers', ['connect:members']);
+grunt.registerTask('testPublic', ['connect:public']);
 
 };
