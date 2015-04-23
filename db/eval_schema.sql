77d07a9d0a4c7b61db3cd53d8b8ff4249ffca862
Travis Whitaker <pi.boy.travis@gmail.com>
2015-04-22 23:15:28 -0400

    Major SQL style changes.
    This commit makes the following style changes:
    
    - Lowercase keywords.
    
    - All identifiers are quoted.
    
    - All constraints except for "not null" are given names.
    
    - All primary keys are bigints.
    
    - PgSQL extensions are not used.
    
    - Foreign keys are added only after all tables are created.
    
    - Objects are dropped if they already exist. This allows the schema to
      be re-initialized without dropping the entire database.
    
    - The entire initialization runs in a single transaction.
    
    This initialization script runs successfully on PostgreSQL 9.4

--- db/eval_schema.sql
+++ db/eval_schema.sql
@@ -1,242 +1,312 @@
--- csh-eval postgresql schema
+-- csh-eval PostgreSQL schema
 -- ==========================
 
--- extensions
-create extension pgcrypto;
-create extension "uuid-ossp";
-create extension citext;
+begin;
 
-create type committee_t as enum (
-    'evals',
-    'rnd',
-    'social',
-    'history',
-    'opcomm',
-    'imps',
-    'financial',
-    'chairman'
+drop type if exists "committee_t" cascade;
+create type "committee_t" as enum (
+    'evals'
+   ,'rnd'
+   ,'social'
+   ,'history'
+   ,'opcomm'
+   ,'imps'
+   ,'financial'
+   ,'chairman'
 );
 
-create type status_t as enum (
-    'pending',
-    'passed',
-    'failed'
+drop type if exists "status_t" cascade;
+create type "status_t" as enum (
+    'pending'
+   ,'passed'
+   ,'failed'
 );
 
-create type member_t as enum (
-    'active',
-    'alumni_good',
-    'alumni_bad',
-    'honorary',
-    'advisory',
-    'introductory',
-    'non'
+drop type if exists "member_t" cascade;
+create type "member_t" as enum (
+    'active'
+   ,'alumni_good'
+   ,'alumni_bad'
+   ,'honorary'
+   ,'advisory'
+   ,'introductory'
+   ,'non'
 );
 
-create type dues_t as enum (
-    'paid',
-    'exempt'
+drop type if exists "dues_t" cascade;
+create type "dues_t" as enum (
+    'paid'
+   ,'exempt'
 );
 
-create type event_t as enum (
-    'house',
-    'social',
-    'committee',
-    'seminar',
-    'orientation'
+drop type if exists "event_t" cascade;
+create type "event_t" as enum (
+    'house'
+   ,'social'
+   ,'committee'
+   ,'seminar'
+   ,'orientation'
 );
 
-create type project_t as enum (
+drop type if exists "project_t" cascade;
+create type "project_t" as enum (
     'major'
 );
 
-create type eval_t as enum (
-    'introductory',
-    'membership'
+drop type if exists "eval_t" cascade;
+create type "eval_t" as enum (
+    'introductory'
+   ,'membership'
 );
 
-create table member (
-    id             serial      primary key,
-    uuid           varchar     default null,
-    username       varchar     not null,
-    commonname     varchar     not null,
-    password       varchar     default null,
-    dues           dues_t      not null default 'unpaid',
-    housing_points integer     not null default 0,
-    on_floor       boolean     not null default false
+drop table if exists "member" cascade;
+create table "member" (
+    "id"              bigserial  primary key
+   ,"uuid"            uuid       default null constraint "unique_uuid" unique
+   ,"username"        varchar    not null constraint "unique_username" unique
+   ,"commonname"      varchar    not null constraint "unique_commonname" unique
+   ,"password_hash"   bytea      default null
+   ,"password_salt"   bytea      default null
+   ,"housing_points"  integer    not null default 0
+   ,"onfloor_status"  boolean    not null default false
 );
 
-create unique index ldapid
-              on member (uuid);
-
-create table eboard (
-    member_id   integer     not null references member (id),
-    committee   committee_t not null,
-    start_date  date        not null,
-    end_date    date        default null
+drop table if exists "eboard" cascade;
+create table "eboard" (
+    "member_id"   bigint       not null
+   ,"committee"   committee_t  not null
+   ,"start_date"  date         not null
+   ,"end_date"    date         default null
 );
 
-create table room (
-    member_id   integer     not null references member (id),
-    room_number varchar     not null,
-    start_date  date        not null,
-    end_date    date        default null
+drop table if exists "room" cascade;
+create table "room" (
+    "member_id"    bigint   not null
+   ,"room_number"  varchar  not null
+   ,"start_date"   date     not null
+   ,"end_date"     date     default null
 );
 
-create table membership (
-    member_id   integer     not null references member (id),
-    status      member_t    not null,
-    start_date  date        not null,
-    end_date    date        default null
+drop table if exists "membership" cascade;
+create table "membership" (
+    "member_id"   bigint    not null
+   ,"status"      member_t  not null
+   ,"start_date"  date      not null
+   ,"end_date"    date      default null
 );
 
-create table event (
-    id          serial      primary key,
-    title       varchar     not null,
-    held        timestamp   not null default now(),
-    category    event_t     not null,
-    committee   committee_t not null,
-    description varchar
+drop table if exists "event" cascade;
+create table "event" (
+    "id"           bigserial    primary key
+   ,"title"        varchar      not null
+   ,"held"         timestamp    not null
+   ,"category"     event_t      not null
+   ,"committee"    committee_t  not null
+   ,"description"  varchar      not null
+   ,constraint "unique_title_held" unique ("title", "held")
 );
 
-create table event_attendee (
-    member_id   integer     not null references member (id),
-    event       integer     not null references event (id),
-    host        boolean     not null default false
+drop table if exists "event_attendee" cascade;
+create table "event_attendee" (
+    "member_id"  bigint   not null
+   ,"event_id"   bigint   not null
+   ,"host"       boolean  not null default false
+   ,constraint "unique_event_attendee" unique ("member_id", "event_id")
 );
 
-create table project (
-    id              serial      primary key,
-    member_id       integer     not null references member (id),
-    title           varchar     not null,
-    description     varchar     not null,
-    submitted       timestamp   not null default now(),
-    passed          timestamp   default null,
-    committee       committee_t not null,
-    project_type    project_t   not null,
-    comments        varchar     default null,
-    status          status_t    not null default 'pending'
+drop table if exists "project" cascade;
+create table "project" (
+    "id"            bigserial    primary key
+   ,"member_id"     bigint       not null
+   ,"title"         varchar      not null
+   ,"description"   varchar      not null
+   ,"submitted"     timestamp    not null
+   ,"passed"        timestamp    default null
+   ,"committee"     committee_t  not null
+   ,"project_type"  project_t    not null
+   ,"comments"      varchar      default null
+   ,"status"        status_t     not null default 'pending'
 );
 
-create table evaluation (
-    id              serial      primary key,
-    member_id       integer     not null references member (id),
-    comments        varchar     default '',
-    deadline        timestamp   not null,
-    status          status_t    not null default 'pending',
-    eval_type       eval_t      not null
+drop table if exists "evaluation" cascade;
+create table "evaluation" (
+    "id"         bigserial  primary key
+   ,"member_id"  bigint     not null
+   ,"comments"   varchar    default null
+   ,"deadline"   timestamp  not null
+   ,"status"     status_t   not null default 'pending'
+   ,"eval_type"  eval_t     not null
 );
 
-create table conditional (
-    id              serial      primary key,
-    evaluation_id   integer     not null references evaluation (id),
-    deadline        timestamp   not null,
-    description     varchar     not null,
-    comments        varchar     default ''
+drop table if exists "conditional" cascade;
+create table "conditional" (
+    "id"             bigserial  primary key
+   ,"evaluation_id"  bigint     not null
+   ,"deadline"       timestamp  not null
+   ,"description"    varchar    not null
+   ,"comments"       varchar    default null
 );
 
-create table freshman_project (
-    id              serial  primary key,
-    description     varchar not null default '',
-    project_date    date    not null default now()
+drop table if exists "freshman_project" cascade;
+create table "freshman_project" (
+    "id"            bigserial  primary key
+   ,"description"   varchar    not null
+   ,"project_date"  date       not null
 );
 
-create table freshman_project_participant (
-    project_id  integer     not null references freshman_project (id),
-    eval_id     integer     not null references evaluation (id),
-    eboard      boolean     not null default false,
-    result      status_t    not null default 'pending',
-    comments    varchar     default ''
+drop table if exists "freshman_project_participant" cascade;
+create table "freshman_project_participant" (
+    "freshman_project_id"  bigint    not null
+   ,"evaluation_id"        bigint    not null
+   ,"eboard"               boolean   not null default false
+   ,"result"               status_t  not null default 'pending'
+   ,"comments"             varchar   default null
 );
 
-create table packet (
-    id          serial      primary key,
-    member_id   integer     references member (id),
-    due_date    date        not null,
-    percent_req integer     not null
+drop table if exists "packet" cascade;
+create table "packet" (
+    "id"           bigserial  primary key
+   ,"member_id"    bigint     not null
+   ,"due_date"     date       not null
+   ,"percent_req"  integer    not null
 );
 
-create table signature (
-    member_id   integer     not null references member (id),
-    packet_id   integer     not null references packet (id),
-    required    boolean     not null default false,
-    signed      timestamp
+drop table if exists "signature" cascade;
+create table "signature" (
+    "member_id"  bigint     not null
+   ,"packet_id"  bigint     not null
+   ,"required"   boolean    not null
+   ,"signed"     timestamp
 );
 
-create table queue (
-    id              serial      primary key,
-    member          integer     references member (id),
-    entered         timestamp   not null default now(),
-    exited          timestamp   default null
+drop table if exists "queue" cascade;
+create table "queue" (
+    "id"         bigserial  primary key
+   ,"member_id"  bigint     not null
+   ,"entered"    timestamp  not null
+   ,"exited"     timestamp  default null
 );
 
-create table application (
-    id              serial      primary key,
-    member          integer     not null references member (id),
-    created         timestamp   not null default now(),
-    status          status_t    not null default 'pending'
+drop table if exists "application" cascade;
+create table "application" (
+    "id"         bigserial  primary key
+   ,"member_id"  bigint     not null
+   ,"created"    timestamp  not null
+   ,"status"     status_t   not null default 'pending'
 );
 
-create table reviewer (
-    member_id       integer     not null references member (id),
-    applicantion_id integer     not null references application (id),
-    review_start    timestamp,
-    revew_submit    timestamp,
-    social          integer,
-    technical       integer,
-    creativity      integer,
-    versatility     integer,
-    leadership      integer,
-    motivation      integer,
-    overall_feel    integer
+drop table if exists "reviewer" cascade;
+create table "reviewer" (
+    "member_id"       bigint     not null
+   ,"application_id"  bigint     not null
+   ,"review_start"    timestamp  not null -- Why do we track both?
+   ,"revew_submit"    timestamp  not null
+   ,"social"          integer    not null -- Maybe these fields should be
+   ,"technical"       integer    not null -- broken out to facilitate changes
+   ,"creativity"      integer    not null -- later on?
+   ,"versatility"     integer    not null
+   ,"leadership"      integer    not null
+   ,"motivation"      integer    not null
+   ,"overall_feel"    integer    not null
 );
 
-create table interviewer (
-    member_id       integer     not null  references member (id),
-    application_id  integer     not null references application (id),
-    interview_date  date        not null default now(),
-    social          integer,
-    technical       integer,
-    creativity      integer,
-    versatility     integer,
-    leadership      integer,
-    motivation      integer,
-    overall_feel    integer
+drop table if exists "interviewer" cascade;
+create table "interviewer" (
+    "member_id"       bigint     not null
+   ,"application_id"  bigint     not null
+   ,"interview_date"  timestamp  not null
+   ,"social"          integer    not null
+   ,"technical"       integer    not null
+   ,"creativity"      integer    not null
+   ,"versatility"     integer    not null
+   ,"leadership"      integer    not null
+   ,"motivation"      integer    not null
+   ,"overall_feel"    integer    not null
 );
 
-create table question (
-    id              serial      primary key,
-    application_id  integer     not null references application (id),
-    query           varchar     not null
+drop table if exists "question" cascade;
+create table "question" (
+    "id"              bigserial  primary key
+   ,"application_id"  bigint     not null
+   ,"query"           varchar    not null
 );
 
-create table answer (
-    application_id  integer     not null references application (id),
-    question_id     integer     not null references question (id),
-    response        varchar     not null
+drop table if exists "answer" cascade;
+create table "answer" (
+    "application_id"  bigint   not null
+   ,"question_id"     bigint   not null
+   ,"response"        varchar  not null
 );
 
-create table housing_eval (
-    id          serial  primary key,
-    eval_date   date    not null
+drop table if exists "housing_eval" cascade;
+create table "housing_eval" (
+    "id"         bigserial  primary key
+   ,"eval_date"  date       not null
 );
 
-create table housing_evaluator (
-    housing_eval_id integer     not null references housing_eval (id),
-    member_id       integer     not null references member (id),
-    score           integer     not null,
-    voted           boolean     not null default false
+drop table if exists "housing_evaluator" cascade;
+create table "housing_evaluator" (
+    "housing_eval_id"  bigint   not null
+   ,"member_id"        bigint   not null
+   ,"score"            integer  not null
+   ,"voted"            boolean  not null default false
 );
 
-create table term (
-    id              varchar    primary key,
-    start_date      date       not null,
-    end_date        date       not null
+drop table if exists "term" cascade;
+create table "term" (
+    "id"          bigint  primary key
+   ,"start_date"  date    not null
+   ,"end_date"    date    default null -- Assume current term.
 );
 
-create table dues (
-    term_id         varchar     not null references term (id),
-    member_id       integer     not null references member (id),
-    status          dues_t      not null
+drop table if exists "dues" cascade;
+create table "dues" (
+    "term_id"    bigint  not null
+   ,"member_id"  bigint  not null
+   ,"status"     dues_t  not null
 );
 
+alter table "eboard" add foreign key ("member_id") references "member" ("id");
+
+alter table "room" add foreign key ("member_id") references "member" ("id");
+
+alter table "membership" add foreign key ("member_id") references "member" ("id");
+
+alter table "event_attendee" add foreign key ("member_id") references "member" ("id");
+alter table "event_attendee" add foreign key ("event_id") references "event" ("id");
+
+alter table "project" add foreign key ("member_id") references "member" ("id");
+
+alter table "evaluation" add foreign key ("member_id") references "member" ("id");
+
+alter table "conditional" add foreign key ("evaluation_id") references "evaluation" ("id");
+
+alter table "freshman_project_participant" add foreign key ("freshman_project_id") references "freshman_project" ("id");
+alter table "freshman_project_participant" add foreign key ("evaluation_id") references "evaluation" ("id");
+
+alter table "packet" add foreign key ("member_id") references "member" ("id");
+
+alter table "signature" add foreign key ("member_id") references "member" ("id");
+alter table "signature" add foreign key ("packet_id") references "packet" ("id");
+
+alter table "queue" add foreign key ("member_id") references "member" ("id");
+
+alter table "application" add foreign key ("member_id") references "member" ("id");
+
+alter table "reviewer" add foreign key ("member_id") references "member" ("id");
+alter table "reviewer" add foreign key ("application_id") references "application" ("id");
+
+alter table "interviewer" add foreign key ("member_id") references "member" ("id");
+alter table "interviewer" add foreign key ("application_id") references "application" ("id");
+
+alter table "question" add foreign key ("application_id") references "application" ("id");
+
+alter table "answer" add foreign key ("application_id") references "application" ("id");
+alter table "answer" add foreign key ("question_id") references "question" ("id");
+
+alter table "housing_evaluator" add foreign key ("housing_eval_id") references "housing_eval" ("id");
+alter table "housing_evaluator" add foreign key ("member_id") references "member" ("id");
+
+alter table "dues" add foreign key ("term_id") references "term" ("id");
+
+commit;
