cd2e145fa1af449140ec88f00055a5b764687325
Travis Whitaker <pi.boy.travis@gmail.com>
2015-10-17 19:14:42 -0400

    Make ghosts exception safe again.

--- src/CSH/Eval/Cacheable/Prim.hs
+++ src/CSH/Eval/Cacheable/Prim.hs
@@ -14,18 +14,27 @@ CSH.Eval.Cacheable.Prim defines primitive operations in the 'CacheM' and
 {-# LANGUAGE FlexibleContexts #-}
 
 module CSH.Eval.Cacheable.Prim (
+    -- * Cache Initialization and Release
+    initCache
+  , releaseCache
     -- * Atomic Reads
-    hitSegment
+  , hitSegment
   , hitRecordFallback
     -- * Fallback Combinators
   , maybeFallback
     -- * Error Reporting
   , noSuchID
+    -- * Ghosts
+  , reaper
+  , singletonGhost
   ) where
 
-import Control.Applicative ((<$>))
+import Control.Applicative ((<$>), (<*>))
 
+import Control.Concurrent
 import Control.Concurrent.MVar
+
+import Control.Exception
 
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.Either
@@ -43,6 +52,50 @@ import Hasql.Postgres
 
 import CSH.Eval.Model
 
+-- | Initialize a 'Cache', including the enclosed 'Pool'.
+initCache :: Settings
+          -> PoolSettings
+          -> IO Cache
+initCache cs ps = let newIDCache = newMVar M.empty
+    in Cache <$>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       newIDCache <*>
+       acquirePool cs ps
+
+-- | Release the 'Pool' enclosed in a 'Cache'.
+releaseCache :: Cache -> IO ()
+releaseCache = releasePool . pool
+
 -- | Atomically read a cache segment.
 hitSegment :: (Cache -> MVar s) -> Cacheable s
 hitSegment = ((liftIO . readMVar) .)
@@ -52,8 +105,10 @@ hitSegment = ((liftIO . readMVar) .)
 hitRecordFallback :: Ord k => k -> M.Map k (MVar v) -> CacheM v -> CacheM v
 hitRecordFallback = ((flip fromMaybe . ((liftIO . readMVar) <$>)) .) . M.lookup
 
--- | Fix this to fork a child to stabilize the cache. Maybe document this at
---   all?
+-- | Create a fallback action in the 'Cacheable' monad around a SQL statement
+--   that returns exactly one or zero result(s). This function does not return
+--   into the 'Maybe' monad; it is named for 'maybeEx' and must be called with
+--   a statement obeying the assumptions 'maybeEx' makes.
 maybeFallback :: CxRow Postgres t
               -- | SQL Statement to execute. The statement must return exactly
               --   zero or one record(s).
@@ -73,3 +128,58 @@ maybeFallback s dbe fr c = do
 -- | Report that a record with the given id in the given table doesn't exist.
 noSuchID :: String -> Word64 -> CacheError
 noSuchID t i = Nonexistent $ t ++ " with id " ++ (show i) ++ " does not exist."
+
+-- | Clear a cache of all it's records. This ain't no LRU. There's probably a
+--   better way to do this. Fix this to be exception safe again.
+reaper :: Cacheable ()
+reaper c = liftIO reapAll
+    where reap = (flip swapMVar) M.empty
+          reapAll = (reap $ memberIDCache c)
+              >> (reap $ eventIDCache c)
+              >> (reap $ projectIDCache c)
+              >> (reap $ evaluationIDCache c)
+              >> (reap $ conditionalIDCache c)
+              >> (reap $ freshmanProjectIDCache c)
+              >> (reap $ packetIDCache c)
+              >> (reap $ queueIDCache c)
+              >> (reap $ applicationIDCache c)
+              >> (reap $ metricIDCache c)
+              >> (reap $ reviewIDCache c)
+              >> (reap $ interviewIDCache c)
+              >> (reap $ questionIDCache c)
+              >> (reap $ termIDCache c)
+              >> (reap $ eboardMemberIDCache c)
+              >> (reap $ roomMemberIDCache c)
+              >> (reap $ membershipMemberIDCache c)
+              >> (reap $ eventAttendeeMemberIDCache c)
+              >> (reap $ eventAttendeeEventIDCache c)
+              >> (reap $ projectParticipantMemberIDCache c)
+              >> (reap $ projectParticipantProjectIDCache c)
+              >> (reap $ freshProjParticipantProjectIDCache c)
+              >> (reap $ freshProjParticipantEvaluationIDCache c)
+              >> (reap $ signatureMemberIDCache c)
+              >> (reap $ signaturePacketIDCache c)
+              >> (reap $ reviewMetricMetricIDCache c)
+              >> (reap $ reviewMetricReviewIDCache c)
+              >> (reap $ interviewMetricMetricIDCache c)
+              >> (reap $ interviewMetricInterviewIDCache c)
+              >> (reap $ answerQuestionIDCache c)
+              >> (reap $ answerApplicationIDCache c)
+              >> (reap $ duesMemberIDCache c)
+              >> return ()
+
+-- | Add a single previously uncached value to the cache. This is done by a new
+--   thread that discards any exceptions, to prevent any cache corruption from
+--   interfering with servicing user requests and to make cache misses less
+--   expensive.
+singletonGhost :: (Cache -> IDCache a)
+               -> Word64
+               -> a
+               -> Cacheable ()
+singletonGhost a k v c = liftIO $ forkFinally insrt1 (\_ -> putStrLn "singletonGhost died!") >> return ()
+    where m1  = a c
+          insrt1 = do
+               bracketOnError (takeMVar m1) (putMVar m1) insrt2
+          insrt2 m2 = case M.lookup k m2 of
+                        Nothing   -> newMVar v >>= (\v' -> putMVar m1 (M.insert k v' m2))
+                        (Just m3) -> swapMVar m3 v >> return ()
