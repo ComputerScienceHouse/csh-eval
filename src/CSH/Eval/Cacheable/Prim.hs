b7ef9d6b8b03fbfa22985fe294224c3c16876f5e
Travis Whitaker <pi.boy.travis@gmail.com>
2015-10-17 02:22:45 -0400

    Add a demonstration fallback combinator.

--- src/CSH/Eval/Cacheable/Prim.hs
+++ src/CSH/Eval/Cacheable/Prim.hs
@@ -11,10 +11,14 @@ CSH.Eval.Cacheable.Prim defines primitive operations in the 'CacheM' and
 'Cacheable' monads.
 -}
 
+{-# LANGUAGE FlexibleContexts #-}
+
 module CSH.Eval.Cacheable.Prim (
     -- * Atomic Reads
     hitSegment
   , hitRecordFallback
+    -- * Fallback Combinators
+  , maybeFallback
     -- * Error Reporting
   , noSuchID
   ) where
@@ -23,6 +27,7 @@ import Control.Applicative ((<$>))
 
 import Control.Concurrent.MVar
 
+import Control.Monad.Trans.Control
 import Control.Monad.Trans.Either
 
 import Control.Monad.IO.Class (liftIO)
@@ -46,6 +51,24 @@ hitSegment = ((liftIO . readMVar) .)
 --   on failure.
 hitRecordFallback :: Ord k => k -> M.Map k (MVar v) -> CacheM v -> CacheM v
 hitRecordFallback = ((flip fromMaybe . ((liftIO . readMVar) <$>)) .) . M.lookup
+
+-- | Fix this to fork a child to stabilize the cache. Maybe document this at
+--   all?
+maybeFallback :: CxRow Postgres t
+              -- | SQL Statement to execute. The statement must return exactly
+              --   zero or one record(s).
+              => Stmt Postgres
+              -- | The error to return if the statement provides no results.
+              -> CacheError
+              -- | Function from the record tuple to the thing you actually
+              --   want.
+              -> (t -> r)
+              -> Cacheable r
+maybeFallback s dbe fr c = do
+    r <- session (pool c) (tx defTxMode (maybeEx s))
+    case r of (Left e)         -> left $ HasqlError e
+              (Right Nothing)  -> left dbe
+              (Right (Just v)) -> right $ fr v
 
 -- | Report that a record with the given id in the given table doesn't exist.
 noSuchID :: String -> Word64 -> CacheError
