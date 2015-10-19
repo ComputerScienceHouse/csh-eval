abe0afbbf86adac88c2b965c0b9ec4016b2d799e
Harlan Haskins <harlan@harlanhaskins.com>
2015-10-19 15:34:48 -0400

    Added redirect for photo URL to redirect to gravatar if LDAP isn't found.

--- src/CSH/Eval/Frontend/ProfilePhoto.hs
+++ src/CSH/Eval/Frontend/ProfilePhoto.hs
@@ -20,10 +20,20 @@ module CSH.Eval.Frontend.ProfilePhoto
 
 import qualified CSH.Eval.LDAP as LD
 import qualified Data.ByteString.Char8 as B
+import Crypto.Hash
 import CSH.Eval.Frontend.Data
 import CSH.Eval.Config
 import Network.Wai
 import Yesod
+
+md5 :: B.ByteString -> Digest MD5
+md5 = hash
+
+hashedEmail :: String -> String
+hashedEmail = B.unpack . digestToHexByteString . md5 . B.pack  . (flip (++)) "@csh.rit.edu"
+
+gravatarURL :: String -> String
+gravatarURL usr = "https://gravatar.com/avatar/" ++ (hashedEmail usr) ++ "?d=mm&size=300"
 
 -- | The handler for the Evaluations Database index page
 getProfilePhotoR :: String -> Handler Html
@@ -31,4 +41,4 @@ getProfilePhotoR usr = do
            photoData <- liftIO $ LD.lookup "jpegPhoto" (B.pack usr)
            case photoData of
                (Just photo) -> sendResponse (typePng, toContent photo)
-               Nothing      -> notFound
+               Nothing      -> redirect $ gravatarURL usr
