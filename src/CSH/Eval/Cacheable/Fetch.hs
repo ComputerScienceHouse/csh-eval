53b0806c5011a61632de2117878383da8092c300
Travis Whitaker <pi.boy.travis@gmail.com>
2015-10-24 17:53:38 -0400

    Bring model into alignment with the cache, finish specifying the remaining fetch functions.

--- src/CSH/Eval/Cacheable/Fetch.hs
+++ src/CSH/Eval/Cacheable/Fetch.hs
@@ -26,14 +26,12 @@ module CSH.Eval.Cacheable.Fetch (
   , getMemberEvaluations
     -- ** Conditional
   , getConditionalID
+  , getEvaluationConditionals
     -- ** FreshmanProject
   , getFreshmanProjectID
     -- ** Packet
   , getPacketID
   , getMemberPackets
-    -- ** Queue
-  , getQueueID
-  , getMemberQueues
     -- ** Application
   , getApplicationID
   , getMemberApplications
@@ -41,8 +39,10 @@ module CSH.Eval.Cacheable.Fetch (
   , getMetricID
     -- ** Review
   , getReviewID
+  , getApplicationReviews
     -- ** Interview
   , getInterviewID
+  , getApplicationInterviews
     -- ** Question
   , getQuestionID
     -- ** Term
@@ -52,19 +52,36 @@ module CSH.Eval.Cacheable.Fetch (
   , getMemberEboards
     -- ** Room
   , getMemberRooms
+    -- ** Queue
+  , getQueueID
+  , getMemberQueues
     -- ** Membership
   , getMemberMemberships
     -- ** EventAttendee
   , getEventEventAttendees
+  , getMemberEventAttendees
     -- ** ProjectParticipant
   , getProjectProjectParticipants
+  , getMemberProjectParticipants
     -- ** FreshmanProjectParticipant
+  , getFreshmanProjectFreshmanProjectParticipants
+  , getMemberFreshmanProjectParticipants
+  , getEvaluationFreshmanProjectParticipants
     -- ** Signature
+  , getPacketSignatures
+  , getMemberSignatures
     -- ** ReviewMetric
+  , getReviewReviewMetrics
+  , getMetricReviewMetrics
     -- ** InterviewMetric
+  , getInterviewInterviewMetrics
+  , getMetricInterviewMetrics
     -- ** Answer
+  , getApplicationAnswers
+  , getQuestionAnswers
     -- ** Dues
   , getMemberDues
+  , getTermDues
     -- * FromRow Functions (Probably shouldn't be exported...)
   , memberFromRow
   , eventFromRow
@@ -80,11 +97,25 @@ module CSH.Eval.Cacheable.Fetch (
   , interviewFromRow
   , questionFromRow
   , termFromRow
+  , eboardFromRow
+  , roomFromRow
+  , membershipFromRow
+  , eventAttendeeFromRow
+  , projectParticipantFromRow
+  , freshmanProjectParticipantFromRow
+  , signatureFromRow
+  , reviewMetricFromRow
+  , interviewMetricFromRow
+  , answerFromRow
+  , duesFromRow
     -- * FromVal Functions (Probably shouldn't be exported...)
-  , eventTypeFromVal
   , committeeFromVal
-  , projectTypeFromVal
+  , evaluationTypeFromVal
   , evaluationStatusFromVal
+  , memberStatusFromVal
+  , duesStatusFromVal
+  , eventTypeFromVal
+  , projectTypeFromVal
   ) where
 
 import Control.Applicative ((<$>))
@@ -161,6 +192,9 @@ getConditionalID i c = do
     right c'
     where db = maybeFallback (getConditionalIDP i) (noSuchID "Conditional" i) conditionalFromRow c
 
+getEvaluationConditionals :: Word64 -> Cacheable [Conditional]
+getEvaluationConditionals = undefined
+
 getFreshmanProjectID :: Word64 -> Cacheable FreshmanProject
 getFreshmanProjectID i c = do
     fc <- hitSegment freshmanProjectIDCache c
@@ -179,17 +213,6 @@ getPacketID i c = do
 
 getMemberPackets :: Word64 -> Cacheable [Packet]
 getMemberPackets = undefined
-
-getQueueID :: Word64 -> Cacheable Queue
-getQueueID i c = do
-    qc <- hitSegment queueIDCache c
-    q  <- hitRecordFallback i qc db
-    singletonGhost queueIDCache i q c
-    right q
-    where db = maybeFallback (getQueueIDP i) (noSuchID "Queue" i) queueFromRow c
-
-getMemberQueues :: Word64 -> Cacheable [Queue]
-getMemberQueues = undefined
 
 getApplicationID :: Word64 -> Cacheable Application
 getApplicationID i c = do
@@ -218,6 +241,9 @@ getReviewID i c = do
     right r
     where db = maybeFallback (getReviewIDP i) (noSuchID "Review" i) reviewFromRow c
 
+getApplicationReviews :: Word64 -> Cacheable [Review]
+getApplicationReviews = undefined
+
 getInterviewID :: Word64 -> Cacheable Interview
 getInterviewID i c = do
     ic <- hitSegment interviewIDCache c
@@ -225,6 +251,9 @@ getInterviewID i c = do
     singletonGhost interviewIDCache i i' c
     right i'
     where db = maybeFallback (getInterviewIDP i) (noSuchID "Interview" i) interviewFromRow c
+
+getApplicationInterviews :: Word64 -> Cacheable [Interview]
+getApplicationInterviews = undefined
 
 getQuestionID :: Word64 -> Cacheable Question
 getQuestionID i c = do
@@ -248,17 +277,70 @@ getMemberEboards = undefined
 getMemberRooms :: Word64 -> Cacheable [Room]
 getMemberRooms = undefined
 
+getQueueID :: Word64 -> Cacheable Queue
+getQueueID i c = do
+    qc <- hitSegment queueIDCache c
+    q  <- hitRecordFallback i qc db
+    singletonGhost queueIDCache i q c
+    right q
+    where db = maybeFallback (getQueueIDP i) (noSuchID "Queue" i) queueFromRow c
+
+getMemberQueues :: Word64 -> Cacheable [Queue]
+getMemberQueues = undefined
+
 getMemberMemberships :: Word64 -> Cacheable [Membership]
 getMemberMemberships = undefined
 
 getEventEventAttendees :: Word64 -> Cacheable [EventAttendee]
 getEventEventAttendees = undefined
 
+getMemberEventAttendees :: Word64 -> Cacheable [EventAttendee]
+getMemberEventAttendees = undefined
+
 getProjectProjectParticipants :: Word64 -> Cacheable [ProjectParticipant]
 getProjectProjectParticipants = undefined
 
+getMemberProjectParticipants :: Word64 -> Cacheable [ProjectParticipant]
+getMemberProjectParticipants = undefined
+
+getFreshmanProjectFreshmanProjectParticipants :: Word64 -> Cacheable [FreshmanProjectParticipant]
+getFreshmanProjectFreshmanProjectParticipants = undefined
+
+getMemberFreshmanProjectParticipants :: Word64 -> Cacheable [FreshmanProjectParticipant]
+getMemberFreshmanProjectParticipants = undefined
+
+getEvaluationFreshmanProjectParticipants :: Word64 -> Cacheable [FreshmanProjectParticipant]
+getEvaluationFreshmanProjectParticipants = undefined
+
+getPacketSignatures :: Word64 -> Cacheable [Signature]
+getPacketSignatures = undefined
+
+getMemberSignatures :: Word64 -> Cacheable [Signature]
+getMemberSignatures = undefined
+
+getReviewReviewMetrics :: Word64 -> Cacheable [ReviewMetric]
+getReviewReviewMetrics = undefined
+
+getMetricReviewMetrics :: Word64 -> Cacheable [ReviewMetric]
+getMetricReviewMetrics = undefined
+
+getInterviewInterviewMetrics :: Word64 -> Cacheable [InterviewMetric]
+getInterviewInterviewMetrics = undefined
+
+getMetricInterviewMetrics :: Word64 -> Cacheable [InterviewMetric]
+getMetricInterviewMetrics = undefined
+
+getApplicationAnswers :: Word64 -> Cacheable [Answer]
+getApplicationAnswers = undefined
+
+getQuestionAnswers :: Word64 -> Cacheable [Answer]
+getQuestionAnswers = undefined
+
 getMemberDues :: Word64 -> Cacheable [Dues]
 getMemberDues = undefined
+
+getTermDues :: Word64 -> Cacheable [Dues]
+getTermDues = undefined
 
 memberFromRow :: (Word64, UUID, T.Text, T.Text, B.ByteString, B.ByteString, Int, Bool) -> Member
 memberFromRow (i, u, un, cn, _, _, hp, os) = Member
@@ -301,53 +383,54 @@ projectFromRow (i, t, d, s, p, c, pt, co, st) = Project
     (evaluationStatusFromVal st)
     (getProjectProjectParticipants i)
 
-evaluationFromRow :: (Word64, T.Text, UTCTime, Bool, T.Text, T.Text) -> Evaluation
-evaluationFromRow (i, c, t, a, s, ty) = Evaluation
+evaluationFromRow :: (Word64, Word64, T.Text, UTCTime, Bool, T.Text, T.Text) -> Evaluation
+evaluationFromRow (i, m, c, t, a, s, ty) = Evaluation
     i
     c
     t
     a
-    s
-    ty
-    (getEvaluationMember i)
+    (evaluationStatusFromVal s)
+    (evaluationTypeFromVal ty)
+    (getMemberID m)
     (getEvaluationConditionals i)
-    (getEvaluationFreshProj i)
+    (getEvaluationFreshmanProjectParticipants i)
 
 conditionalFromRow :: (Word64, Word64, UTCTime, T.Text, T.Text) -> Conditional
-conditionalFromRow (i, t, d, c) = Conditional
+conditionalFromRow (i, e, t, d, c) = Conditional
     i
     t
     d
     c
+    (getEvaluationID e)
 
 freshmanProjectFromRow :: (Word64, T.Text, Word64, Word64) -> FreshmanProject
 freshmanProjectFromRow (i, d, t, e)= FreshmanProject
     i
     d
-    t
-    e
-    (getFreshProjParticipants i)
+    (getTermID t)
+    (getEventID e)
+    (getFreshmanProjectFreshmanProjectParticipants i)
 
-packetFromRow :: (Word64, Day, Int) -> Packet
-packetFromRow (i, d, f) = Packet
+packetFromRow :: (Word64, Word64, Day, Int) -> Packet
+packetFromRow (i, m, d, f) = Packet
     i
     d
     f
-    (getPacketMember i)
+    (getMemberID m)
     (getPacketSignatures i)
 
-queueFromRow :: (Word64, UTCTime) -> Queue
-queueFromRow (i, t) = Queue
-    i
-    t
-    (getQueueMember i)
+queueFromRow :: (Word64, UTCTime, Maybe UTCTime) -> Queue
+queueFromRow (m, en, ex) = Queue
+    en
+    ex
+    (getMemberID m)
 
-applicationFromRow :: (Word64, UTCTime, T.Text) -> Application
-applicationFromRow (i, t, s) = Application
+applicationFromRow :: (Word64, Word64, UTCTime, T.Text) -> Application
+applicationFromRow (i, m, t, s) = Application
     i
     t
-    s
-    (getApplicationMember i)
+    (evaluationStatusFromVal s)
+    (getMemberID m)
     (getApplicationReviews i)
     (getApplicationInterviews i)
     (getApplicationAnswers i)
@@ -358,25 +441,25 @@ metricFromRow (i, n, a) = Metric
     n
     a
 
-reviewFromRow :: (Word64, UTCTime, UTCTime) -> Review
-reviewFromRow (i, s, f) = Review
+reviewFromRow :: (Word64, Word64, Word64, UTCTime, UTCTime) -> Review
+reviewFromRow (i, m, a, s, f) = Review
     i
     s
     f
-    (getReviewMember i)
-    (getReviewApplication i)
-    (getReviewMetrics i)
+    (getMemberID m)
+    (getApplicationID a)
+    (getReviewReviewMetrics i)
 
-interviewFromRow :: (Word64, UTCTime) -> Interview
-interviewFromRow (i, t) = Interview
+interviewFromRow :: (Word64, Word64, Word64, UTCTime) -> Interview
+interviewFromRow (i, m, a, t) = Interview
     i
     t
-    (getInterviewMember i)
-    (getInterviewApplication i)
-    (getInterviewMetrics i)
+    (getMemberID m)
+    (getApplicationID a)
+    (getInterviewInterviewMetrics i)
 
 questionFromRow :: (Word64, Bool, T.Text) -> Question
-questionFromRow (i, t, a) = Question
+questionFromRow (i, a, t) = Question
     i
     t
     a
@@ -387,13 +470,77 @@ termFromRow (i, s, e) =Term
     s
     e
 
--- | This function is partial! Explain why...
-eventTypeFromVal :: T.Text -> EventType
-eventTypeFromVal "house"       = HouseMeeting
-eventTypeFromVal "social"      = SocialEvent
-eventTypeFromVal "committee"   = CommitteeMeeting
-eventTypeFromVal "seminar"     = Seminar
-eventTypeFromVal "orientation" = Orientation
+eboardFromRow :: (Word64, T.Text, Day, Maybe Day) -> Eboard
+eboardFromRow (m, c, s, e) = Eboard
+    (committeeFromVal c)
+    s
+    e
+    (getMemberID m)
+
+roomFromRow :: (Word64, T.Text, Day, Day) -> Room
+roomFromRow (m, r, s, e) = Room
+    r
+    s
+    e
+    (getMemberID m)
+
+membershipFromRow :: (Word64, T.Text, Day, Maybe Day) -> Membership
+membershipFromRow (m, st, s, e) = Membership
+    (memberStatusFromVal st)
+    s
+    e
+    (getMemberID m)
+
+eventAttendeeFromRow :: (Word64, Word64, Bool) -> EventAttendee
+eventAttendeeFromRow (m, e, h) = EventAttendee
+    h
+    (getMemberID m)
+    (getEventID e)
+
+projectParticipantFromRow :: (Word64, Word64, T.Text) -> ProjectParticipant
+projectParticipantFromRow (m, p, d) = ProjectParticipant
+    d
+    (getMemberID m)
+    (getProjectID p)
+
+freshmanProjectParticipantFromRow :: (Word64, Word64, Bool, T.Text, T.Text) -> FreshmanProjectParticipant
+freshmanProjectParticipantFromRow (f, ev, eb, s, c) = FreshmanProjectParticipant
+    eb
+    (evaluationStatusFromVal s)
+    c
+    (getFreshmanProjectID f)
+    (getEvaluationID ev)
+
+signatureFromRow :: (Word64, Word64, Bool, Maybe UTCTime) -> Signature
+signatureFromRow (m, p, r, s) = Signature
+    r
+    s
+    (getMemberID m)
+    (getPacketID p)
+
+reviewMetricFromRow :: (Word64, Word64, Int) -> ReviewMetric
+reviewMetricFromRow (m, r, s) = ReviewMetric
+    s
+    (getMetricID m)
+    (getReviewID r)
+
+interviewMetricFromRow :: (Word64, Word64, Int) -> InterviewMetric
+interviewMetricFromRow (m, i, s) = InterviewMetric
+    s
+    (getMetricID m)
+    (getInterviewID i)
+
+answerFromRow :: (Word64, Word64, T.Text) -> Answer
+answerFromRow (a, q, r) = Answer
+    r
+    (getQuestionID q)
+    (getApplicationID a)
+
+duesFromRow :: (Word64, Word64, T.Text) -> Dues
+duesFromRow (m, t, s) = Dues
+    (duesStatusFromVal s)
+    (getMemberID m)
+    (getTermID t)
 
 -- | This function is partial! Explain why...
 committeeFromVal :: T.Text -> Committee
@@ -401,17 +548,44 @@ committeeFromVal "evals"     = Evals
 committeeFromVal "rnd"       = RnD
 committeeFromVal "social"    = Social
 committeeFromVal "history"   = History
-committeeFromVal "opcomm"    = OpCom
+committeeFromVal "opcomm"    = OpComm
 committeeFromVal "imps"      = Imps
 committeeFromVal "financial" = Imps
 committeeFromVal "chairmam"  = Chairman
 
 -- | This function is partial! Explain why...
-projectTypeFromVal :: T.Text -> ProjectType
-projectTypeFromVal "major" = Major
+evaluationTypeFromVal :: T.Text -> EvaluationType
+evaluationTypeFromVal "introductory" = IntroEval
+evaluationTypeFromVal "membership"   = MembershipEval
 
 -- | This function is partial! Explain why...
 evaluationStatusFromVal :: T.Text -> EvaluationStatus
 evaluationStatusFromVal "pending" = Pending
 evaluationStatusFromVal "passed"  = Passed
 evaluationStatusFromVal "failed"  = Failed
+
+-- | This function is partial! Explain why...
+memberStatusFromVal :: T.Text -> MemberStatus
+memberStatusFromVal "active"       = Active
+memberStatusFromVal "alumni_good"  = AlumniGood
+memberStatusFromVal "alumni_bad"   = AlumniBad
+memberStatusFromVal "honorary"     = Honorary
+memberStatusFromVal "advisory"     = Advisory
+memberStatusFromVal "introductory" = Intro
+memberStatusFromVal "non"          = Non
+
+-- This function is partial! Explain why...
+duesStatusFromVal :: T.Text -> DuesStatus
+duesStatusFromVal "paid"   = Paid
+duesStatusFromVal "evempt" = Exempt
+
+-- | This function is partial! Explain why...
+eventTypeFromVal :: T.Text -> EventType
+eventTypeFromVal "house"       = HouseMeeting
+eventTypeFromVal "social"      = SocialEvent
+eventTypeFromVal "committee"   = CommitteeMeeting
+eventTypeFromVal "seminar"     = Seminar
+eventTypeFromVal "orientation" = Orientation
+-- | This function is partial! Explain why...
+projectTypeFromVal :: T.Text -> ProjectType
+projectTypeFromVal "major" = Major
